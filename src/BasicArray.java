c043da6e0ccde962d72ae596808360d0c571c1e2
shayla <shayla8002@gmail.com>
2024-01-11 12:51:29 -0800

    added code

--- src/BasicArray.java
+++ src/BasicArray.java
@@ -16,5 +16,16 @@ public class BasicArray
         }
         System.out.println();
 
+        //added code
+        String name = "SHAYLA";
+        int limit = name.length();
+        char[] letters = new char[limit];
+
+        for (int index = 0; index < limit; index++)
+        {
+            letters[index] = name.charAt(index);
+            System.out.print(letters[index] + " ");
+        }
+        System.out.println();
     }
 }
